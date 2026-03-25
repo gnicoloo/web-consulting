a9837d89db6ab83805f5b28df40d64a56801cc54
Giuseppe Nicolo <nicologiuseppe87@gmail.com>
2026-03-25 10:22:07 +0100

    try to fix image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,20 +6,20 @@ export const COLORS = {
   whatsapp: '#25D366'
 };
 
-export const IMAGES = [
-  { 
-    url: "/images/image1.png", // Il percorso parte da 'public'
-    title: "Sicurezza sul Lavoro",
-    desc: "Valutazione rischi e ingegneria della prevenzione."
-  },
-  { 
-    url: "/images/image2.png", 
-    title: "Ingegneria del Rischio",
-    desc: "Analisi tecnica avanzata per la continuità operativa."
-  },
-  { 
-    url: "/images/image.png", 
-    title: "Formazione Tecnica",
-    desc: "Corsi certificati e sviluppo competenze 4.0."
-  }
-];
+// export const IMAGES = [
+//   { 
+//     url: "/images/image1.png", // Il percorso parte da 'public'
+//     title: "Sicurezza sul Lavoro",
+//     desc: "Valutazione rischi e ingegneria della prevenzione."
+//   },
+//   { 
+//     url: "/images/image2.png", 
+//     title: "Ingegneria del Rischio",
+//     desc: "Analisi tecnica avanzata per la continuità operativa."
+//   },
+//   { 
+//     url: "/images/image.png", 
+//     title: "Formazione Tecnica",
+//     desc: "Corsi certificati e sviluppo competenze 4.0."
+//   }
+// ];
